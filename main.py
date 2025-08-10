9fd0a0c81ff89b190f03fe93ab4a4ad00b8ec85a
Anoop Betageri <anbet.9900@gmail.com>
2025-08-10 08:41:43 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import nltk
+
+def main():
+    print("Hello from weekend-ai-project!")
+    print("Download nltk....")
+    nltk.download('punkt')
+    nltk.download('vader_lexicon')
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+
+
+if __name__ == "__main__":
+    main()
+
